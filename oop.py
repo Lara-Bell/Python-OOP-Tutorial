aa7dd4d91b00daf2c0a09f98338a0434e337c01a
Lara_Bell <j8cwsqe29e2p@gmail.com>
2018-08-04 16:09:08 +0900

    Python OOP Tutorial 5: Special (Magic/Dunder) Methods

--- oop.py
+++ oop.py
@@ -16,50 +16,27 @@ class Employee:
   def apply_raise(self):
     self.pay = int(self.pay * self.raise_amt)
 
-class Developer(Employee):
-  raise_amt = 1.10
+  def __repr__(self):
+    return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
 
-  def __init__ (self, first, last, pay, prog_lang):
-    super().__init__(first, last, pay)
-    self.prog_lang = prog_lang
+  def __str__(self):
+    return '{} - {}'.format(self.fullname(), self.email)
 
-class Manager(Employee):
-  def __init__ (self, first, last, pay, employees=None):
-    super().__init__(first, last, pay)
-    if employees is None:
-      self.employees = []
-    else:
-      self.employees = employees
+  def __add__(self, other):
+    return self.pay + other.pay
 
-  def add_emp(self, emp):
-    if emp not in self.employees:
-      self.employees.append(emp)
+  def __len__(self):
+    return len(self.fullname())
 
-  def remove_emp(self, emp):
-    if emp in self.employees:
-      self.employees.remove(emp)
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'Employee', 60000)
 
-  def print_emps(self):
-    for emp in self.employees:
-      print('-->', emp.fullname())
+print(len(emp_1))
 
-dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
-dev_2 = Developer('Test', 'User', 60000, 'Java')
+# print(emp_1)
 
-mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+# print(repr(emp_1))
+# print(str(emp_1))
 
-print(issubclass(Manager, Developer))
-
-# print(mgr_1.email)
-
-# mgr_1.add_emp(dev_2)
-# mgr_1.remove_emp(dev_1)
-
-# mgr_1.print_emps()
-
-# print(dev_1.email)
-# print(dev_1.prog_lang)
-
-# print(dev_1.pay)
-# dev_1.apply_raise()
-# print(dev_1.pay)
+# print(emp_1.__repr__())
+# print(emp_1.__str__())
