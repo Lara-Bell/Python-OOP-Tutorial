e637089f5f802775c189cc038c083d445bcb3d83
Lara_Bell <j8cwsqe29e2p@gmail.com>
2018-08-04 15:34:53 +0900

    Python OOP Tutorial 4: Inheritance - Creating Subclasses

--- oop.py
+++ oop.py
@@ -2,7 +2,6 @@
 
 class Employee:
 
-  num_of_emps = 0
   raise_amt = 1.04
 
   def __init__ (self, first, last, pay):
@@ -11,35 +10,56 @@ class Employee:
     self.email = first + '.' + last + '@company.com'
     self.pay = pay
 
-    Employee.num_of_emps += 1
-
   def fullname(self):
     return '{0} {1}'.format(self.first, self.last)
 
   def apply_raise(self):
     self.pay = int(self.pay * self.raise_amt)
 
-  @classmethod
-  def set_raise_amt(cls, amount):
-    cls.raise_amt = amount
+class Developer(Employee):
+  raise_amt = 1.10
 
-  @classmethod
-  def from_string(cls, emp_str):
-    first, last, pay = emp_str.split('-')
-    return cls(first, last, pay)
+  def __init__ (self, first, last, pay, prog_lang):
+    super().__init__(first, last, pay)
+    self.prog_lang = prog_lang
 
-  @staticmethod
-  def is_workday(day):
-    if day.weekday() == 5 or day.weekday() == 6:
-      return False
-    return True
+class Manager(Employee):
+  def __init__ (self, first, last, pay, employees=None):
+    super().__init__(first, last, pay)
+    if employees is None:
+      self.employees = []
+    else:
+      self.employees = employees
 
+  def add_emp(self, emp):
+    if emp not in self.employees:
+      self.employees.append(emp)
 
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test', 'User', 60000)
+  def remove_emp(self, emp):
+    if emp in self.employees:
+      self.employees.remove(emp)
 
-import datetime
+  def print_emps(self):
+    for emp in self.employees:
+      print('-->', emp.fullname())
 
-my_date = datetime.date(2016, 7, 11)
+dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
+dev_2 = Developer('Test', 'User', 60000, 'Java')
 
-print(Employee.is_workday(my_date))
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+print(issubclass(Manager, Developer))
+
+# print(mgr_1.email)
+
+# mgr_1.add_emp(dev_2)
+# mgr_1.remove_emp(dev_1)
+
+# mgr_1.print_emps()
+
+# print(dev_1.email)
+# print(dev_1.prog_lang)
+
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
